4ff4294bd1f7fdf42dd483347ec9bfc6fdbe8e69
Jia Xuan Ng <jiaxuanng42@gmail.com>
2022-01-05 21:19:17 +0000

    feature: Add Day 6 implementation
    - - Added Day 6 solution: both tasks completed. Won't lie, this one bamboozled me because I wasn't thinking properly and just wrote a bunch of code. Then I realised what it was actually asking! :cry:

---
+++ 2021/src/main/java/com/jxng1/days/Day6.java
@@ -0,0 +1,53 @@
+package main.java.com.jxng1.days;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Day6 extends Day {
+    public Day6(int day) {
+        super(day);
+    }
+
+    @Override
+    String task1(List<String> input) {
+        var ret = Arrays
+                .stream(input.toString().replaceAll("(\\[)|(\\])", "").split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        return String.valueOf(simulate(ret, 80));
+    }
+
+    @Override
+    String task2(List<String> input) {
+        var ret = Arrays
+                .stream(input.toString().replaceAll("(\\[)|(\\])", "").split(","))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+
+        return String.valueOf(simulate(ret, 256));
+    }
+
+    private long simulate(int[] input, int days) {
+        long[] generations = new long[9];
+        Arrays.fill(generations, 0);
+
+        for (int fish : input) {
+            generations[fish]++;
+        }
+
+        // only need to keep track of the cycles of fish, not the fish themselves...
+        for (int i = 1; i <= days; i++) {
+            long oldGen = generations[0]; // ones about to spawn a new generation
+
+            for (int j = 1; j < generations.length; j++) {
+                generations[j - 1] = generations[j]; // we shuffle them forward
+            }
+            generations[6] += oldGen; // these are the ones that have spawned new generations
+            generations[8] = oldGen; // these are the ones that have just spawned.
+        }
+
+        return Arrays.stream(generations).sum();
+    }
+}
