f0430053d42c04c104f728821e76814ce733cb0c
Jia Xuan Ng <jiaxuanng42@gmail.com>
2022-01-09 15:27:00 +0000

    feature: Add new features to Day
    - Added timing support for output.
    - Added colour support for output.

--- 2021/src/main/java/com/jxng1/days/Day.java
+++ 2021/src/main/java/com/jxng1/days/Day.java
@@ -7,12 +7,31 @@ import java.util.stream.Collectors;
 
 public abstract class Day {
 
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
     public Day(int day) {
         List<String> input = InputReader.getInputReader().getInputAsList("day" + day + ".txt");
 
-        System.out.println("Day: " + day);
-        System.out.println("Task 1 Output: " + task1(input));
-        System.out.println("Task 2 Output: " + task2(input));
+        System.out.println(ANSI_YELLOW + "Day: " + ANSI_PURPLE + day + ANSI_RESET);
+        long task1Start = System.currentTimeMillis();
+        String task1Output = task1(input);
+        long task1End = System.currentTimeMillis();
+        System.out.println("Task 1 Output: " + ANSI_RED + task1Output + ANSI_RESET);
+        System.out.println("Task 1 Time: " + ANSI_GREEN + (task1End - task1Start) + "ms" + ANSI_RESET);
+
+        long task2Start = System.currentTimeMillis();
+        String task2Output = task2(input);
+        long task2End = System.currentTimeMillis();
+        System.out.println("Task 2 Output: " + ANSI_RED + task2Output + ANSI_RESET);
+        System.out.println("Task 2 Time: " + ANSI_GREEN + (task2End - task2Start) + "ms" + ANSI_RESET);
         System.out.println("---");
     }
 
