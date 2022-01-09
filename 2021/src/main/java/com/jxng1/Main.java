c03033796786471f4c0c04f15fc3ccdfe030ff17
Jia Xuan Ng <jiaxuanng42@gmail.com>
2022-01-09 15:25:42 +0000

    feature: Add Day 10 implementation
    - Added Day 10 implementation: reminded me of the shunting-yard algorithm :+1:

--- 2021/src/main/java/com/jxng1/Main.java
+++ 2021/src/main/java/com/jxng1/Main.java
@@ -15,7 +15,7 @@ public class Main {
         new Day7(7);
         new Day8(8);
         new Day9(9);
-//        new Day10(10);
+        new Day10(10);
 //        new Day11(11);
 //        new Day12(12);
 //        new Day13(13);
